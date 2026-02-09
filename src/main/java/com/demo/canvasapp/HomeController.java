9308baa80a7b818161dc85343ff7ac878cf4e27a
521qinmi <521qinmi@163.com>
2026-02-09 12:40:46 +0800

    Update HomeController.java

--- src/main/java/com/demo/canvasapp/HomeController.java
+++ src/main/java/com/demo/canvasapp/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
@@ -19,5 +21,6 @@ public class HomeController {
         return "home";
     }
 }
+
 
 
